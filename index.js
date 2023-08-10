fe8bebe4db82768376319ecdb618c8e69840bc2b
RanCwb <henriqsantos629@gmail.com>
2023-08-10 12:25:12 -0300

    newUp

--- index.js
+++ index.js
@@ -7,51 +7,49 @@ let navBar = document.querySelector(".navbar");
 var header = document.getElementById("header");
 var prevScrollPos = window.pageYOffset;
 window.onscroll = () => {
-    sections.forEach((sec) => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach((links) => {
-                links.classList.remove("active");
-                document
-                    .querySelector("header nav a [href*=' + id + ']")
-                    .classList.add("active");
-            });
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a [href*=' + id + ']")
+          .classList.add("active");
+      });
+    }
+  });
 
- 
-
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window, scrollY > 100);
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window, scrollY > 100);
 };
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x");
-    menuIcon.classList.toggle("active");
+  menuIcon.classList.toggle("bx-x");
+  menuIcon.classList.toggle("active");
 };
 
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.pageYOffset;
 
-    if (prevScrollPos > currentScrollPos) {
-        header.style.top = "0";
-    } else {
-        header.style.top = "-100px";
-    }
+  if (prevScrollPos > currentScrollPos) {
+    header.style.top = "0";
+  } else {
+    header.style.top = "-100px";
+  }
 
-    prevScrollPos = currentScrollPos;
+  prevScrollPos = currentScrollPos;
 };
 
 function downloadCurriculo() {
-    var link = document.createElement("a");
-    link.href = "./src/cv.prho.pdf"; // Caminho para o currículo dentro da pasta src
-    link.download = "curriculo.pdf"; // Nome do arquivo que será baixado
+  var link = document.createElement("a");
+  link.href = "./src/curriculum/Curriculo RanathanHr.pdf"; // Caminho para o currículo dentro da pasta src
+  link.download = "curriculo.pdf"; // Nome do arquivo que será baixado
 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
